4da74b303a18c5baa27958ea927cb0d3b374afcb
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-11-02 20:27:45 -0400

    Update functional.py

--- functional.py
+++ functional.py
@@ -1,3 +1,18 @@
 def flatten_and_sort(arr):
     out = []
-    
+    for item in arr:
+        if type(item) == list:
+            for i in item:
+                out.append(i)
+        else:
+            out.append(item)
+
+
+
+    return sorted(out)
+
+first_collection = [1, [55, 17, 12],3, [22, 19, 35], 9,2]
+print(flatten_and_sort(first_collection))
+print(first_collection)
+
+
